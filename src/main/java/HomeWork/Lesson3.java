52fd6155858bcdf42419236ce1cd7109ebf47c41
Smolinetti <2407373@mail.ru>
2022-05-05 13:33:00 +0300

    Lesson_3_2

--- src/main/java/HomeWork/Lesson3.java
+++ src/main/java/HomeWork/Lesson3.java
@@ -1,9 +1,17 @@
 package HomeWork;
 
+import java.util.Arrays;
+
 public class Lesson3 {
     public static void main(String[] args) {
         final int SEASON_NUMBER = 3;
+
         seasons(SEASON_NUMBER);
+        arraysPrint();
+        arraysFor();
+        doubleArrays();
+        arraysPrintDouble();
+
 
     }
 
@@ -18,6 +26,48 @@ public class Lesson3 {
 
         System.out.println(season);
 
+    }
+
+    private static void arraysPrint() {
+        String arr[] = {"A", "B", "C", "D", "E"};
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void arraysFor() {
+        int arr[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+    }
+
+    private static void doubleArrays() {
+        int count = 1;
+        int table[][] = new int[3][4];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                table[i][j] = count;
+                System.out.print(table[i][j] + " ");
+                count++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+    public static void arraysPrintDouble() {
+        int count = 10;
+        int[][] arr = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = count;
+                count = count + 10;
+            }
+        }
+        System.out.println(Arrays.deepToString(arr));
+        System.out.println();
     }
 
 }
